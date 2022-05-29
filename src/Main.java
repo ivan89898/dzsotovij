506485c3f22d2289b014d0b12c32a639e8c026bd
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-05-30 02:41:46 +0500

    C:\Users\chizh\IdeaProjects\sotovij\src\Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        double startingCount = 10.00; // началькое количество рублей на текущем счёте клиента
         double refill = 1500.00; // сумма, на которую клиент пополнил счёт
         double bonus = 0.01; // бонус
         if ((refill > 1000.00)) {
@@ -8,7 +7,9 @@ public class Main {
         } else {
             System.out.println("Не начислено бонусных рублей");
         }
-        System.out.println("Итоговый счёт:" + (startingCount + refill+ refill * bonus) + " рублей");
+        // началькое количество рублей на текущем счёте клиента
+        double startingCount = 10.00;
+        System.out.println("Итоговый счёт:" + (startingCount + refill + refill * bonus) + " рублей");
 
     }
 }
